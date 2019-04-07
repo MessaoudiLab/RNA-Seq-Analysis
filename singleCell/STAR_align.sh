9063239d3f20a31338c30854e286cd0cb4c08c51
MessaoudiLab <42880500+MessaoudiLab@users.noreply.github.com>
2019-04-06 18:31:49 -0700

    Update STAR_align.sh

--- singleCell/STAR_align.sh
+++ singleCell/STAR_align.sh
@@ -21,6 +21,6 @@ cd $SLURM_SUBMIT_DIR
 
 # Align
 for i in *.read1_val_1.fq.gz; do
-        /bigdata/messaoudilab/arivera/Scripts/STAR-2.5.3a/bin/Linux_x86_64/STAR --runMode alignReads --genomeLoad NoSharedMemory --limitBAMsortRAM 15000000000 --outSAMtype BAM SortedByCoordinate --outSAMmapqUnique 255 --readFilesCommand zcat --genomeDir /bigdata/messaoudilab/arivera/Reference_genomes/Rhesus_SVV/STAR_genome_62 --runThreadN 10 --readFilesIn $i ${i%.read1_val_1.fq.gz}.read2_val_2.fq.gz --outFileNamePrefix ${i%.read1_val_1.fq.gz}
+        /bigdata/messaoudilab/arivera/Scripts/STAR-2.5.3a/bin/Linux_x86_64/STAR --runMode alignReads --genomeLoad NoSharedMemory --limitBAMsortRAM 15000000000 --outSAMtype BAM SortedByCoordinate --outSAMmapqUnique 255 --readFilesCommand zcat --genomeDir /bigdata/messaoudilab/arivera/Reference_genomes/Rhesus_SVV/STAR_genome --runThreadN 10 --readFilesIn $i ${i%.read1_val_1.fq.gz}.read2_val_2.fq.gz --outFileNamePrefix ${i%.read1_val_1.fq.gz}
 done
 

fd1e4ad098a6d1cc0851e0b1d06a556ec15cdab3
MessaoudiLab <42880500+MessaoudiLab@users.noreply.github.com>
2019-06-02 22:02:57 -0700

    Create example_download.sh

---
+++ Pre-processing/Downloading Data/example_download.sh
@@ -0,0 +1,6 @@
+wget http://illumina.bioinfo.ucr.edu/illumina_runs/358/flowcell34_lane99_pair1_CGATGT.fastq.gz
+wget http://illumina.bioinfo.ucr.edu/illumina_runs/358/flowcell34_lane99_pair1_ATGCAT.fastq.gz
+wget http://illumina.bioinfo.ucr.edu/illumina_runs/358/flowcell34_lane99_pair1_CATATG.fastq.gz
+wget http://illumina.bioinfo.ucr.edu/illumina_runs/358/flowcell34_lane99_pair1_CCGTTA.fastq.gz
+wget http://illumina.bioinfo.ucr.edu/illumina_runs/358/flowcell34_lane99_pair1_TTAGGC.fastq.gz
+wget http://illumina.bioinfo.ucr.edu/illumina_runs/358/flowcell34_lane99_pair1_ATCACG.fastq.gz
